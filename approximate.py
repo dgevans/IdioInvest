Fixed derivatives with respect to p
Signed-off-by: David Evans <dgevans42@gmail.com>

approximate.py
@@ -491,7 +491,7 @@ class approximate(object):
         
         d[v,S],d[v,eps],d[v,Z] = Ivy.dot(d[y,S]) + Ivy.dot(dy[Z](z_i)).dot(IZYhat).dot(d[Y,S]), Ivy.dot(dy[z](z_i)).dot(Izy).dot(dy[eps](z_i)), Ivy.dot(dy[Z](z_i)).dot(self.dZ_Z)
         d[v,Eps] = Ivy.dot(dy[z](z_i).dot(Izy).dot(dy[Eps](z_i)) + dy[Y](z_i).dot(self.dYGamma_Eps) + dy[Z](z_i).dot(IZYhat).dot(self.dY_Eps) )
-        d[v,p] =  Ivy.dot( dy[z](z_i).dot(Izy).dot(dy[p](z_i)) + dy[Y](z_i).dot(self.dYGamma_p) + dy[Z](z_i).dot(IZYhat).dot(self.dY_p) )      
+        d[v,p] =  Ivy.dot( dy[z](z_i).dot(Izy).dot(dy[p](z_i)) + dy[Y](z_i).dot(self.dYGamma_p) + dy[Z](z_i).dot(IZYhat).dot(self.dY_p) + d[y,p])      
         
         d[eps,S],d[eps,eps],d[eps,Z],d[eps,Eps],d[eps,p] = np.zeros((neps,nz+nY)),np.eye(neps),np.zeros((neps,nZ)),np.zeros((neps,nEps)),np.zeros((neps,n_p))
         
@@ -820,6 +820,25 @@ class approximate(object):
         self.d2y[Z,Eps] = dict_fun(lambda z_i: ytild_ZEps(z_i) + np.tensordot(ytild_YZEps(z_i),self.d2Y[Z,Eps],1))
         self.d2y[Eps,Z] = lambda z_i: self.d2y[Z,Eps](z_i).transpose(0,2,1)
         
+    def testpZ(self):
+        z_i = self.Gamma[0]
+        zprime_p,Zprime_p = Izy.dot(self.dy[p](z_i)),IZYhat.dot(self.dY_p)
+        Zprime_Z = self.dZ_Z
+        Sprime_p = np.vstack((zprime_p,self.dY(z_i).dot(zprime_p)))
+        zprime_pZ1 = np.tensordot(Izy,self.ytild_pZ(z_i),1)
+        zprime_pZ2 = np.tensordot(Izy,np.einsum('ijk,jlk->ilk',self.ytild_YpZ(z_i),self.d2Y[p,Z]),1)
+        Zprime_pZ = np.tensordot(IZYhat,self.d2Y[p,Z],1)
+        ret = []
+        ret.append(np.tensordot(self.dy[z](z_i),zprime_pZ1,1)[-1] + np.tensordot(self.dy[Y](z_i).dot(self.dY(z_i)),zprime_pZ1,1)[-1]
+        +quadratic_dot(self.d2y[S,Z](z_i),Sprime_p,Zprime_Z)[-1]+quadratic_dot(self.d2y[Z,Z](z_i),Zprime_p,Zprime_Z)[-1]
+        +np.einsum('ijk,jlk,lm,kp->imp',self.dy[Y,S,Z](z_i),self.d2Y[z,Z](z_i),zprime_p,Zprime_Z)[-1]
+        +np.einsum('ijk,kl->ijl',self.ytild_pZ(z_i),Zprime_Z)[-1]
+        )
+        ret.append(np.tensordot(self.dy[z](z_i),zprime_pZ2,1)[-1] + np.tensordot(self.dy[Y](z_i).dot(self.dY(z_i)),zprime_pZ2,1)[-1]
+        +np.tensordot(self.dy[Z](z_i),Zprime_pZ,1)[-1] + +np.einsum('ijk,jlk,km->ilm',self.ytild_YpZ(z_i),self.d2Y[p,Z],Zprime_Z)[-1]
+        )
+        return ret
+        
         
     def compute_d2p(self):
         '''
@@ -877,15 +896,17 @@ class approximate(object):
             
         ytild_pZ = dict_fun(lambda z_i: a_pZ(z_i) + np.einsum('ijk,jlk->ilk',c_pZ(z_i),temp1_pZ))
         ytild_YpZ = dict_fun(lambda z_i: b_pZ(z_i) + np.einsum('ijk,jlk->ilk',c_pZ(z_i),temp2_pZ))
+        self.ytild_pZ = ytild_pZ
+        self.ytild_YpZ = ytild_YpZ
         
         #sovling for YpZ
         DG_pZ = self.integrate(lambda z_i : HGhat(z_i,p,Z) + np.tensordot(self.DG(z_i)[nG:,y],ytild_pZ(z_i),1)  )
         DG_YpZ = self.integrate(lambda z_i : self.DG(z_i)[nG:,Y,np.newaxis] +np.tensordot(self.DG(z_i)[nG:,y],ytild_YpZ(z_i),1) )
         self.d2Y[p,Z] = np.empty((nY,n_p,nZ))
         for i in range(nZ):
-            self.d2Y[p,Z][:,:,i] = np.linalg.solve(DG_YpZ[:,:,i],-DG_pZ[:,:,i])
-        self.d2y[p,Z] = dict_fun(lambda z_i :ytild_pZ(z_i) + np.einsum('ijk,jlk->ilk',ytild_YpZ(z_i),self.d2Y[p,Z]) )
-        
+            temp = np.linalg.inv(DG_YpZ[:,:,i])
+            self.d2Y[p,Z][:,:,i] = np.tensordot(temp,-DG_pZ[:,:,i],1)
+        self.d2y[p,Z] = dict_fun(lambda z_i: ytild_pZ(z_i) + np.einsum('ijk,jlk->ilk',ytild_YpZ(z_i),self.d2Y[p,Z]) )
         
         '''Now w.r.t p and Gamma'''
         def DF_ytild_pG_inv(z_i):

calibrations/calibrate_ramsey_taxrate.py
@@ -7,7 +7,7 @@ Created on Thu Apr 17 12:18:27 2014
 import numpy as np
 import pycppad as ad
 
-beta = 0.95
+beta = 0.96
 gamma = 2.
 sigma = 1.5
 #sigma_e = np.array([0.04,0.05,0.1,0.05])
@@ -169,7 +169,7 @@ def G(w):
     ret[9] = B_ - b_
     ret[10] = logm-0.
     
-    ret[11] = T+Gov#- (tau_k*pi + tau_l*W*w_e*l - Gov)
+    ret[11] = T + Gov
     ret[12] = K_ - k_
     ret[13] = foc_tau_k
     ret[14] = foc_R
@@ -299,7 +299,7 @@ def GSS(YSS,y_i,weights):
     c,l,k_,nl,m = np.exp(logc),np.exp(logl),np.exp(logk_),np.exp(lognl),np.exp(logm)
     mu = muhat*m
     K = np.exp(logK_)   
-    Uc = c**(-sigma)    
+    Uc = c**(-sigma)   
     
     return np.hstack(
     [weights.dot(K - k_), weights.dot(f - c - Gov - K), weights.dot(l*w_e - nl), weights.dot((pi-k_)*mu*Uc + rho3_/beta+pi*Zeta), weights.dot(W*w_e*l*Uc*mu + phi1 + W*w_e*l*Zeta),
@@ -329,7 +329,7 @@ def check_extreme(z_i):
     extreme = False
     if z_i[0] < -3. or z_i[0] > 6.:
         extreme = True
-    if z_i[1] > 5. or z_i[1] < -5.:
+    if z_i[1] > 8. or z_i[1] < -8.:
         extreme = True
     return extreme
     